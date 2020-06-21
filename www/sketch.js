77b14501ff42bcd7c425cc2368b05f2f0d64fda1
muramari97 <22016044@hiroshima.kosen-ac.jp>
2020-06-22 02:25:00 +0900

    当たり判定追加

--- www/sketch.js
+++ www/sketch.js
@@ -1,19 +1,40 @@
 let balloon; // 風船
 let haretsu; //破裂した風船
+let balsize = 100;
 let x;
 let y;
+let bombBal = false;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   balloon = loadImage('sozai/balloon.png');
   haretsu = loadImage('sozai/haretsu.png');
-  x = random(width/5);
+  x = random(width);
   y = height;
 }
 
 function draw() {
-  background(255);
-  x = random(-20,20);
-  y -= 3;
-  image(balloon,x,y,100,100);
+  if(!bombBal){
+    background(255);
+    x = random(-20,20);
+    y -= 3;
+    //当たり判定
+    if (
+      mouseX > x - balsize &&
+      mouseX < x + balsize &&
+      mouseY > y - balsize &&
+      mouseY < y + balsize
+    ) {
+      if (mouseIsPressed) {
+        image(haretsu,x,y,balsize,balsize);
+        bombBal = true;
+      }else{
+        image(balloon,x,y,balsize,balsize);
+      }
+    }else{
+      image(balloon,x,y,balsize,balsize);
+    }
+  }
+
+
 }
