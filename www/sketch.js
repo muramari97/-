90e43ee429369cdcf896984c000e65e2e12805a9
muramari97 <22016044@hiroshima.kosen-ac.jp>
2020-06-22 02:42:05 +0900

    10回のカウントを追加

--- www/sketch.js
+++ www/sketch.js
@@ -1,40 +1,50 @@
 let balloon; // 風船
 let haretsu; //破裂した風船
 let balsize = 100;
-let x;
+let x = [];
 let y;
 let bombBal = false;
+let count = 0;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   balloon = loadImage('sozai/balloon.png');
   haretsu = loadImage('sozai/haretsu.png');
-  x = random(width);
+  for(let i=0;i<10;i++){
+    x[i] = random(width);
+  }
   y = height;
 }
 
 function draw() {
+  
   if(!bombBal){
     background(255);
-    x = random(-20,20);
+    x[count] += random(-10,10);
     y -= 3;
     //当たり判定
     if (
-      mouseX > x - balsize &&
-      mouseX < x + balsize &&
+      mouseX > x[count] - balsize &&
+      mouseX < x[count] + balsize &&
       mouseY > y - balsize &&
       mouseY < y + balsize
     ) {
       if (mouseIsPressed) {
-        image(haretsu,x,y,balsize,balsize);
+        image(haretsu,x[count],y,balsize,balsize);
         bombBal = true;
+        count += 1;
       }else{
-        image(balloon,x,y,balsize,balsize);
+        image(balloon,x[count],y,balsize,balsize);
       }
     }else{
-      image(balloon,x,y,balsize,balsize);
+      image(balloon,x[count],y,balsize,balsize);
     }
   }
-
+  
+  //10回繰り返す
+  if(bombBal && count < 10){
+    y = height;
+    bombBal = false;
+  }
 
 }
